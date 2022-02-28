5276632695c29604ecd1977ca950d051e586fdd8
Andre Santos <1170852@isep.ipp.pt>
2022-02-28 03:37:05 +0000

    Initial commit

--- alert.py
+++ alert.py
@@ -13,9 +13,9 @@ def email_alert(subject,body,to):
     msg['subject'] = subject
     msg['to'] = to
         
-    user = "undisputedbikesharing@gmail.com"
+    user = "YOUR EMAIL HERE"
     msg['from'] = user
-    password = "BO69rqe0eJi8"
+    password = "YOUR PASSWORD HERE"
         
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.starttls()
@@ -34,8 +34,8 @@ while(True):
     for div in data.findAll('div',attrs={'header-defcon-level header-defcon-level-news-regions'}):
         text = div.text.strip().partition('\n')[0]
         if '(one)' in text.lower():
-            email_alert("URGENT - NUCLEAR","DEFCON LEVEL 1 ACTIVATED","dekas.santos2@gmail.com")
-            email_alert("URGENT - NUCLEAR","DEFCON LEVEL 1 ACTIVATED","joana.filipa.queiros@hotmail.com")
+            email_alert("URGENT - NUCLEAR","DEFCON LEVEL 1 ACTIVATED","DESTINATION EMAIL HERE")
+            #email_alert("URGENT - NUCLEAR","DEFCON LEVEL 1 ACTIVATED","....")
             
     print("Everything is calm.")
     time.sleep(900)
